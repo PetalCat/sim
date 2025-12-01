eec68512cc140280f1f0990c36edf00dda9b9ed6
ParkerH27 <hasenkap@student.psdr3.org>
2025-12-01 10:17:45 -0600

    feat: use in-memory database for client when building

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,6 +6,7 @@ import { env } from '$env/dynamic/private';
 
 if (!building && !env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = createClient({ url: env.DATABASE_URL || 'file:local.db' });
+const url = building ? ':memory:' : env.DATABASE_URL || 'file:local.db';
+const client = createClient({ url });
 
 export const db = drizzle(client, { schema });
